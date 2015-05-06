fe0e7aae7cee6d42ac3f744e8b780a479dd888b3
tamas <agile.abduction@gmail.com>
2015-05-06 02:16:06 +0200

    silex

--- public/index.php
+++ public/index.php
@@ -1,21 +1,47 @@
 <?php
-	require('../vendor/autoload.php');
-	$Aconv = new \A54conv();
-	$input = isset($_REQUEST['input']) && is_string($_REQUEST['input']) ? $_REQUEST['input'] : null;
-	$output = $Aconv->convert($input);
-?><!doctype html>
-<html>
-<head>
-	<style>
-		textarea { width: 90%; height: 300px; }
-	</style>
-</head>
-<body>
-	<form method="POST">
-		<pre>input:</pre>
-		<textarea name="input"><?= $input ?></textarea>&nbsp;<input type="submit"><br/><br/>
-		<pre>output:</pre>
-		<textarea><?= $output ?></textarea><br/>
-	</form>
-</body>
-</html>
+
+use Symfony\Component\HttpFoundation\Response;
+
+require('../vendor/autoload.php');
+
+	try {
+
+		$Aconv = new \A54conv();
+
+		$App = new Silex\Application();
+
+		$App->register(new Silex\Provider\TwigServiceProvider(), array(
+				'twig.path' => __DIR__.'/../views',
+			));
+
+		$App->get(
+			'/convert',
+			function() use ($App) {
+				return $App['twig']->render('convert.twig');
+			}
+		);
+
+		$App->post(
+			'/convert',
+			function() use ($App, $Aconv) {
+				return new Response(
+					json_encode(['success'=>1, 'result'=>$Aconv->convert('asd')])
+				);
+			}
+		);
+
+		$App->get(
+			'/README',
+			function() use ($App, $Aconv) {
+				// this gives a bad error
+				return '<pre>' . file_get_contents('../readme.md');
+			}
+		);
+
+		$App->run();
+
+	}
+	catch (\Exception $e) {
+		// why I don't end up here??
+		die ('oops: ' . $e->getMessage());
+	}
